caa4e5f0aa02fb91b72a683f098f3cb0931ab896
Ian <ianramosc@gmail.com>
2015-07-28 19:57:14 -0300

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp')
+	,	jade = require('gulp-jade')
+	,	stylus = require('gulp-stylus');
+
+gulp.task('html', function() {
+	gulp.src('./dev/jade/*.jade')
+		.pipe(jade({
+			locals: './'
+		}))
+		.pipe(gulp.dest('./'))
+});
+
+gulp.task('css', function() {
+	gulp.src('./dev/css/**/*.styl')
+		.pipe(stylus({
+			compress: true
+		}))
+		.pipe(gulp.dest('./stylesheets'))
+});
+
+gulp.task('default', ['html', 'css'], function() {
+	gulp.watch('./dev/jade/*.jade', 	['html']);
+	gulp.watch('./dev/css/**/*.styl', ['css']);
+});
